ffa3b4a5ddac4179881dadf5846175bba0899a2d
Mike West <mikewesthub@github.com>
2024-08-08 19:29:16 -0400

    Add Organization validation specs

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Test for validations
+  it "is valid with valid attributes" do
+    organization = Organization.new(name: "Test Organization", address: "123 Test St")
+    expect(organization).to be_valid
+  end
+
+  xit "is not valid without a name" do
+    organization = Organization.new(name: nil, address: "123 Test St")
+    expect(organization).to_not be_valid
+  end
+
+  xit "is not valid without an address" do
+    organization = Organization.new(name: "Test Organization", address: nil)
+    expect(organization).to_not be_valid
+  end
 end
